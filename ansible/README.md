74f7616d012e0ac152cda242017f862ef33bf031
adshidtadka <nextage900fx@gmail.com>
2020-09-01 22:45:32 +0900

    ansibleと_atomを削除

--- ansible/README.md
+++
@@ -1,9 +0,0 @@
-# dotfiles ansible
-
-## Usage
-
-```
-cd ~/dotfiles/ansible
-curl https://raw.githubusercontent.com/adshidtadka/dotfiles/master/ansible/init.sh | sh
-./provision.sh
-```
--- ansible/provision.sh
+++
@@ -1,5 +0,0 @@
-SCRIPT_DIR=$(dirname "$0")
-cd $SCRIPT_DIR
-export HOMEBREW_CASK_OPTS="--appdir=/Applications"
-ansible-galaxy install -r requirements.yml
-ansible-playbook -v -K -i hosts playbook.yml
