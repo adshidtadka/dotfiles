451de2cbb2347b10dc90d2c74f08516cb0ec8a15
adshidtadka <nextage900fx@gmail.com>
2020-09-07 10:05:51 +0900

    ディレクトリを変更

--- _bashrc/deploy.sh
+++ _bashrc/deploy.sh
@@ -6,19 +6,6 @@ echo "deploying .bashrc ..."
 
 cd $DOTPATH
 
-if sed --version 2>/dev/null |grep -q GNU;then
-  alias sedi='sed -i"" '
-else
-  alias sedi='sed -i "" '
-fi
-
-if ! [ -e "$HOME"/.bashrc ]; then
-  touch "$HOME"/.bashrc
-fi
-
-sedi "/##### begin dotfiles #####/,/##### end dotfiles #####/c\\" "$HOME"/.bashrc
-
-
 echo "##### begin dotfiles #####" >> "$HOME"/.bashrc
 
 _x_sh_shs=`ls $DOTPATH/_x_shrc/[0-9][0-9]_*.sh`
