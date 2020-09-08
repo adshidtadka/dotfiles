7c40de5bb82497def6e5c3f59b2d46039eb936b8
adshidtadka <nextage900fx@gmail.com>
2020-09-09 01:45:20 +0900

    使っていないaliasを削除

--- _x_shrc/02_aliases.sh
+++ _x_shrc/02_aliases.sh
@@ -9,36 +9,6 @@ alias rm='rm -i'
 alias cp='cp -i'
 alias mv='mv -i'
 
-# dotfiles
-alias dotfiles="cd $DOTPATH"
-alias srcrc="if [ -e ~/.${SHELL##*/}rc ]; then . ~/.${SHELL##*/}rc; fi"
-
-# emacs
-alias emacs='emacs -nw'
-
-# gcc
-alias g++14='g++ -std=c++14 -Wall'
-
-# ruby, rails
-alias be='bundle exec'
-alias rsb='bundle exec rails s -b 0.0.0.0'
-alias rtcc='bundle exec rake tmp:cache:clear'
-alias Prc='bundle exec rails c -e production'
-alias Prap='RAILS_ENV=production bundle exec rake assets:precompile assets:clean'
-alias Prsb='bundle exec rails s -b 0.0.0.0 -e production'
-
-# Log
-zstyle -s ':prezto:module:git:log:medium' format '_git_log_medium_format' \
-  || _git_log_medium_format='%C(bold)Commit:%C(reset) %C(green)%H%C(red)%d%n%C(bold)Author:%C(reset) %C(cyan)%an <%ae>%n%C(bold)Date:%C(reset)   %C(blue)%ai (%ar)%C(reset)%n%+B'
-zstyle -s ':prezto:module:git:log:oneline' format '_git_log_oneline_format' \
-  || _git_log_oneline_format='%C(green)%h%C(reset) %s%C(red)%d%C(reset)%n'
-zstyle -s ':prezto:module:git:log:brief' format '_git_log_brief_format' \
-  || _git_log_brief_format='%C(green)%h%C(reset) %s%n%C(blue)(%ar by %an)%C(red)%d%C(reset)%n'
-
-# Status
-zstyle -s ':prezto:module:git:status:ignore' submodules '_git_status_ignore_submodules' \
-  || _git_status_ignore_submodules='none'
-
 # Git
 alias g='git'
 
@@ -302,19 +272,12 @@ alias vast='vagrant status'
 # docker
 alias dcm='docker-compose'
 
-# linux
-alias osstats='cat /etc/redhat-release /proc/version /proc/cpuinfo /proc/meminfo && echo "\n=====\n" && lscpu && echo "\n=====\n" && df -h'
-
 # local IP address 確認
 alias localips="ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*'"
 
 # redis
 alias rds='redis-server'
 alias rdc='redis-cli'
-
-# npm
-alias nrbw='npm run build:watch'
-alias wpbw='$(npm bin)/webpack --progress --colors --watch'
 
 # tail
 alias tlf='tail -F'
@@ -328,23 +291,3 @@ alias nuo='nkf -Luw --overwrite'
 # python
 alias jnb='jupyter notebook'
 
-# fasd
-eval "$(fasd --init auto)"
-alias a='fasd -a'        # any
-alias s='fasd -si'       # show / search / select
-alias d='fasd -d'        # directory
-alias f='fasd -f'        # file
-alias sd='fasd -sid'     # interactive directory selection
-alias sf='fasd -sif'     # interactive file selection
-alias z='fasd_cd -d'     # cd, same functionality as j in autojump
-alias zz='fasd_cd -d -i' # cd with interactive selection
-
-# peco
-function peco-history-selection() {
-    BUFFER=`history -n 1 | tail -r  | awk '!a[$0]++' | peco`
-    CURSOR=$#BUFFER
-    zle reset-prompt
-}
-
-zle -N peco-history-selection
-bindkey '^R' peco-history-selection
