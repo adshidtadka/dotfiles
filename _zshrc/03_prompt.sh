7d5b31eabc6c5d4b7d078a63c67e89f537ca287b
adshidtadka <nextage900fx@gmail.com>
2020-09-09 01:57:06 +0900

    小文字でも補完が効く

--- _zshrc/03_prompt.sh
+++ _zshrc/03_prompt.sh
@@ -2,3 +2,6 @@
 
 # complement
 autoload -Uz compinit && compinit
+
+ # 通常補完 -> （小文字 -> 大文字） -> （小文字 -> 大文字 + 大文字 -> 小文字)
+zstyle ':completion:*' matcher-list '' 'm:{[:lower:]}={[:upper:]}' '+m:{[:upper:]}={[:lower:]}'
