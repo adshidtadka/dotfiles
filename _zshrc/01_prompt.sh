66a52aa7e7e6ba357e9f4da87be36318a130dcf9
adshidtadka <nextage900fx@gmail.com>
2020-09-09 01:45:57 +0900

    zsh用のaliasを移動

--- _zshrc/01_prompt.sh
+++ _zshrc/01_prompt.sh
@@ -1,1 +1,4 @@
+#!/bin/sh
+
+# complement
 autoload -Uz compinit && compinit
---
+++ _zshrc/02_aliases.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# fasd
+eval "$(fasd --init auto)"
+alias a='fasd -a'        # any
+alias s='fasd -si'       # show / search / select
+alias d='fasd -d'        # directory
+alias f='fasd -f'        # file
+alias sd='fasd -sid'     # interactive directory selection
+alias sf='fasd -sif'     # interactive file selection
+alias z='fasd_cd -d'     # cd, same functionality as j in autojump
+alias zz='fasd_cd -d -i' # cd with interactive selection
+
+# peco
+function peco-history-selection() {
+    BUFFER=`history -n 1 | tail -r  | awk '!a[$0]++' | peco`
+    CURSOR=$#BUFFER
+    zle reset-prompt
+}
+
+zle -N peco-history-selection
+bindkey '^R' peco-history-selection
