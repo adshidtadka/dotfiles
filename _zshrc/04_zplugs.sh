1398ff0b447db99b01e911d48e2954ebdf8e28a6
adshidtadka <nextage900fx@gmail.com>
2020-09-09 03:04:23 +0900

    zplugのプラグインを読み込む

--- _zshrc/04_zplugs.sh
+++ _zshrc/04_zplugs.sh
@@ -3,3 +3,14 @@
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 
 zplug "zsh-users/zsh-autosuggestions", defer:2
+
+# インストールする
+if ! zplug check --verbose; then
+    printf "Install? [y/N]: "
+    if read -q; then
+        echo; zplug install
+    fi
+fi
+
+# プラグインを読み込み、コマンドにパスを通す
+zplug load --verbose
