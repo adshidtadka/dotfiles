e3d087ce1c904c6dd2e0f49a95dd1255580640a2
adshidtadka <nextage900fx@gmail.com>
2020-09-09 02:19:39 +0900

    ホスト名を表示

--- _zshrc/03_prompt.sh
+++ _zshrc/03_prompt.sh
@@ -7,9 +7,8 @@ autoload -Uz compinit && compinit
 zstyle ':completion:*' matcher-list '' 'm:{[:lower:]}={[:upper:]}' '+m:{[:upper:]}={[:lower:]}'
 
 # 色を使用出来るようにする
-autoload -Uz colors
-colors
+autoload -Uz colors && colors
 
 # 2行表示
-PROMPT="%{${fg[$zshrc_prompt_color]}%}%n@%M:%{${reset_color}%}%~
+PROMPT="%{${fg[$zshrc_prompt_color]}%}%n@%m:%{${reset_color}%}%~
 %{%(?.$fg[white].$fg[red])%}%#%{$reset_color%} "
