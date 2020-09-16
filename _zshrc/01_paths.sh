c114d910be58236713d58147811786accd6f4018
adshidtadka <nextage900fx@gmail.com>
2020-09-16 23:30:14 +0900

    zplugのパスをOSごとにかえる

--- _zshrc/01_paths.sh
+++ _zshrc/01_paths.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
 # zplug
-export ZPLUG_HOME=/usr/local/opt/zplug
+#!/bin/sh
+
+if command_exists sw_vers ; then
+    export ZPLUG_HOME=/usr/local/opt/zplug
+else
+    export ZPLUG_HOME="$HOME"/.zplug/zplug
+fi
 source $ZPLUG_HOME/init.zsh
