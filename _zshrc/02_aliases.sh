eab1b5061f307eb8801019573af91a390d61a89a
taksawa <taksawa@yahoo-corp.jp>
2020-09-16 16:41:23 +0900

    exa fasdコマンドが存在するかを確認

--- _zshrc/02_aliases.sh
+++ _zshrc/02_aliases.sh
@@ -17,21 +17,25 @@ alias glg='git log --topo-order --all --graph --pretty=format:"${_git_log_onelin
 alias glb='git log --topo-order --pretty=format:"${_git_log_brief_format}"'
 
 # exa
-alias ls='exa --icons'
+if command_exists exa ; then
+  alias ls='exa --icons'
+fi
 alias ll='ls -lh'
 alias la='ls -lah'
 alias lt='ls -lah --tree'
 
 # fasd
-eval "$(fasd --init auto)"
-alias a='fasd -a'        # any
-alias s='fasd -si'       # show / search / select
-alias d='fasd -d'        # directory
-alias f='fasd -f'        # file
-alias sd='fasd -sid'     # interactive directory selection
-alias sf='fasd -sif'     # interactive file selection
-alias z='fasd_cd -d'     # cd, same functionality as j in autojump
-alias zz='fasd_cd -d -i' # cd with interactive selection
+if command_exists fasd ; then
+  eval "$(fasd --init auto)"
+  alias a='fasd -a'        # any
+  alias s='fasd -si'       # show / search / select
+  alias d='fasd -d'        # directory
+  alias f='fasd -f'        # file
+  alias sd='fasd -sid'     # interactive directory selection
+  alias sf='fasd -sif'     # interactive file selection
+  alias z='fasd_cd -d'     # cd, same functionality as j in autojump
+  alias zz='fasd_cd -d -i' # cd with interactive selection
+fi
 
 # peco
 function peco-history-selection() {
