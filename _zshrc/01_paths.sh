0948632d0b6a784bcba67d440b5913a433a55f38
taksawa <taksawa@yahoo-corp.jp>
2020-09-23 09:58:19 +0900

    deoplate, neoplateを使わない

--- _zshrc/01_paths.sh
+++ _zshrc/01_paths.sh
@@ -6,6 +6,6 @@
 if command_exists sw_vers ; then
     export ZPLUG_HOME=/usr/local/opt/zplug
 else
-    export ZPLUG_HOME="$HOME"/.zplug/zplug
+    export ZPLUG_HOME="$HOME"/.zplug
 fi
 source $ZPLUG_HOME/init.zsh
