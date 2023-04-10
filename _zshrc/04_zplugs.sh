080a332d60afef37d0a53e176a29020e701d045d
adshidtadka <nextage900fx@gmail.com>
2023-04-11 08:36:43 +0900

    revert Zsh Autosuggestions

--- _zshrc/04_zplugs.sh
+++ _zshrc/04_zplugs.sh
@@ -2,6 +2,9 @@
 
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 
+zplug "zsh-users/zsh-autosuggestions", defer:2
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#4c4c4c"
+
 # インストールする
 if ! zplug check --verbose; then
     printf "Install? [y/N]: "
